591bc1ccfe76215f632497be8f2458d1abbcadd3
André Dias <39736248+andrefcdias@users.noreply.github.com>
2024-11-01 20:32:41 +0100

    feat: make CLI a joy
    also add posibility to define the speed of the animation

--- README.md
+++ README.md
@@ -7,14 +7,22 @@ WebP, with Sharp and ImageMagick.
 | ------ | ---------------------------------------------------------- |
 | After  | ![animated green dinosaur walking](test.webp)              |
 
+## Requisites
+
+The app is built with Deno, so you need to have it installed to run the app.
+
+[Installing Deno](https://docs.deno.com/runtime/getting_started/installation/)
+
 ## Usage
 
 The app accepts both local paths or http uris that point to a file.
 
-`deno run -A main.ts <path or uri> <...>`
+`deno run -A main.ts [flags] <path or uri>`
 
-## Examples
+## Flags
 
-`deno run -A main.ts /cool-sprite.png`
+### --frameDelay
 
-`deno run -A main.ts https://coolurl.com/cool-sprite.png`
+Default: 3 (30ms).
+
+*Sets the delay between each frame of the generated animation. The delay is set in 1/100th of a second (1 = 10ms).*
--- main.ts
+++ main.ts
@@ -6,6 +6,49 @@ import {
   MagickImage,
   MagickImageCollection,
 } from "https://deno.land/x/imagemagick_deno@0.0.31/mod.ts";
+import { parseArgs } from "@std/cli";
+
+const sendHelp = (error?: string) => {
+  if (error) {
+    console.log(`%c${error}`, "color: red");
+  }
+
+  console.log(`
+Usage: deno run -A main.ts [options] <image>
+      
+Flags:
+  --frameDelay <ms>  The delay between frames in 1/100th of a second. Default is 10 (100ms).
+  --help             Show this help message.
+`);
+
+  Deno.exit(error ? 1 : 0);
+};
+
+const parseArguments = () => {
+  const args = parseArgs(Deno.args, {
+    boolean: ["help"],
+    string: ["frameDelay"],
+    default: { frameDelay: "3" },
+  });
+
+  if (args.help) {
+    sendHelp();
+  }
+
+  if (args._.length !== 1) {
+    sendHelp("Image path or URI was not passed as an argument.");
+  }
+
+  const frameDelay = parseInt(args.frameDelay);
+  if (isNaN(frameDelay) || frameDelay < 0) {
+    sendHelp("frameDelay must be a positive integer.");
+  }
+
+  return {
+    imagePath: args._[0].toString(),
+    frameDelay,
+  };
+};
 
 const readFile = async (path: string) => {
   try {
@@ -26,7 +69,7 @@ const readFile = async (path: string) => {
   return await Deno.readFile(path);
 };
 
-const parseImage = async (image: ArrayBuffer) => {
+const parseImage = async (image: ArrayBuffer, frameDelay: number) => {
   const sharpImage = sharp(image);
   const metadata = await sharpImage.metadata();
   if (!metadata.height || !metadata.width) {
@@ -51,7 +94,7 @@ const parseImage = async (image: ArrayBuffer) => {
 
     const cropped = await sharpImage.clone().extract(region).toBuffer();
     const magickImage = MagickImage.create(new Uint8Array(cropped));
-    magickImage.animationDelay = 5;
+    magickImage.animationDelay = frameDelay;
     magickImage.gifDisposeMethod = GifDisposeMethod.Previous;
     frameCollection.push(magickImage);
   }
@@ -62,14 +105,11 @@ const parseImage = async (image: ArrayBuffer) => {
 const main = async () => {
   await initialize();
 
-  const imagePath = Deno.args[0];
-  if (!imagePath) {
-    throw new Error("No image path provided");
-  }
+  const { imagePath, frameDelay } = parseArguments();
 
   const imageContents = await readFile(imagePath);
 
-  const frameCollection = await parseImage(imageContents);
+  const frameCollection = await parseImage(imageContents, frameDelay);
 
   frameCollection.write(MagickFormat.WebP, (img) =>
     Deno.writeFileSync("test.webp", img)
